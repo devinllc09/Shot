402296da0c90ef23b28a218d79ff2ca0b4252bba
devinllc09 <incrypto09@gmail.com>
2023-11-17 06:33:23 -0800

    addede shot

---
+++ app/src/main/java/com/devin/shot/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.devin.shot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homes);
+    }
+}
